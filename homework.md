1629c7bf260726fdd14f8f939c9a0c5820d33348
akshaychavan23031998 <akshayrchavan07@gmail.com>
2025-03-29 17:34:31 +0530

    Routing, GET, PST, DELETE.

--- homework.md
+++ homework.md
@@ -6,3 +6,17 @@ Episode 3rd: Creating Express Server:
 4) listen to port 7777 or which ever u want
 5) write request handler for / and /test
 6) install nodemon and update the package.json scripts of start and dev.
+
+  Episode 4th: Routing and request handler.
+
+Initalize github ==>> git init
+
+.gitignore file
+
+create a remote repo on github.
+
+push all code to remote origin.
+
+play with route extensins ex. /, /test, /home, /home/2 and etc...
+
+Order of the route matters alot
--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
-// console.log("Dev Tinder is Ready to good to Gooo.....
-
 const express = require('express');
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("Welcome to /test route");
+
+app.get("/user", (req, res) => {        //this below get call will only match /user get call
+    res.send({firstname: "Akshay", lastname: "Chavan"})
 })
 
-app.use((req, res) => {
+app.post("/user", (req, res) => {        //this below post call will only match /user get call
+    res.send("Saved the Data to the DB")
+})
+
+app.delete("/user", (req, res) => {        //this below delete call will only match /user get call
+    res.send("deleted Data from the DB")
+})
+
+
+app.use("/test", (req, res) => {        //this match all the HTTP Method API Calls to /test 
     res.send("Welcome to / route");
 })
+
 
 app.listen(7777, () => {
     console.log('Dev Tinder is running on port 7777');
