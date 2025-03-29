ad372e25524d640a40c5648c904a7825c218e9a5
akshaychavan23031998 <akshayrchavan07@gmail.com>
2025-03-29 17:59:25 +0530

    advanced Routing techniqueies.

--- src/app.js
+++ src/app.js
@@ -2,24 +2,22 @@ const express = require('express');
 
 const app = express();
 
-
-app.get("/user", (req, res) => {        //this below get call will only match /user get call
+//this will work for usr and user as i have done qustion mark infront of e so it will become optional. 
+app.get("/use?r", (req, res) => {        //this below get call will only match /user get call
     res.send({firstname: "Akshay", lastname: "Chavan"})
 })
 
-app.post("/user", (req, res) => {        //this below post call will only match /user get call
-    res.send("Saved the Data to the DB")
+//this is work for user and useeeeeer,and etc as i have added plus sign infront of e, so it will add mire stings infront of e.
+app.get("/use+r", (req, res) => {        //this below get call will only match /user get call
+    res.send({firstname: "Akshay", lastname: "Chavan"})
 })
 
-app.delete("/user", (req, res) => {        //this below delete call will only match /user get call
-    res.send("deleted Data from the DB")
+// this will work for user and we have added star * infront of s soo * means anything, ex: usAKSHAYRAMCHAVANWERTer.
+app.get("/us*er", (req, res) => {        //this below get call will only match /user get call
+    res.send({firstname: "Akshay", lastname: "Chavan"})
 })
 
-
-app.use("/test", (req, res) => {        //this match all the HTTP Method API Calls to /test 
-    res.send("Welcome to / route");
-})
-
+//these are complex regex of route and can be use combined as well.
 
 app.listen(7777, () => {
     console.log('Dev Tinder is running on port 7777');
