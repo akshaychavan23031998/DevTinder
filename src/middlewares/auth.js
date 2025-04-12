ef41874510575b5c1748b1591c277938839ad255
akshaychavan23031998 <akshayrchavan07@gmail.com>
2025-04-12 19:49:28 +0530

    S2EP17(DevTinder UI Part 3): Impoved the userAuth middle ware.

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -11,7 +11,7 @@ const userAuth = async (req, res, next) => {
     const {token} = req.cookies;
 
     if(!token) {
-        throw new Error("Time Out, Please Login Again!");
+        return res.status(401).send("Please Login!!");
     }
 
     const decodedObj = await jwt.verify(token, "devTinder@123");
